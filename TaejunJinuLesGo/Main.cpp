56770129cad48bdbed146b9584fac43875a755da
rgnjjw <rgn7jjw@gmail.com>
2026-06-23 09:03:37 +0900

    c

--- TaejunJinuLesGo/Main.cpp
+++ TaejunJinuLesGo/Main.cpp
@@ -8,7 +8,167 @@
 #include "Skill.h"
 #include "Card.h"
 
-static void RunGame()
+#include <cstdlib>
+#include <string>
+
+namespace
+{
+    enum class GameResult
+    {
+        GameOver,
+        GameClear,
+        ExitGame
+    };
+
+    enum class GameOverAction
+    {
+        Retry,
+        MainMenu,
+        ExitProgram
+    };
+
+    bool HasActiveArrow()
+    {
+        for (int i = 0; i < MAX_ARROWS; ++i)
+        {
+            if (arrows[i].active)
+                return true;
+        }
+
+        return false;
+    }
+
+    int GetRemainingTargetCount()
+    {
+        int remaining = 0;
+
+        for (int i = 0; i < targetCount; ++i)
+        {
+            if (targets[i].active)
+                ++remaining;
+        }
+
+        return remaining;
+    }
+
+    bool IsGameOver()
+    {
+        return ammo <= 0 && !HasActiveArrow() && !AllTargetsCleared();
+    }
+
+    void DrawText(int x, int y, Color color, const string& text)
+    {
+        GotoXY(x, y);
+        SetColor(color);
+        cout << text;
+        SetColor();
+    }
+
+    void DrawCenteredText(int y, Color color, const string& text)
+    {
+        int x = (CONSOLE_WIDTH - (int)text.length()) / 2;
+        if (x < 1) x = 1;
+
+        DrawText(x, y, color, text);
+    }
+
+    void DrawBox(int x, int y, int width, int height, Color color)
+    {
+        SetColor(color);
+
+        GotoXY(x, y);
+        cout << '+';
+        for (int i = 0; i < width - 2; ++i) cout << '-';
+        cout << '+';
+
+        for (int row = 1; row < height - 1; ++row)
+        {
+            GotoXY(x, y + row);
+            cout << '|';
+
+            GotoXY(x + width - 1, y + row);
+            cout << '|';
+        }
+
+        GotoXY(x, y + height - 1);
+        cout << '+';
+        for (int i = 0; i < width - 2; ++i) cout << '-';
+        cout << '+';
+
+        SetColor();
+    }
+
+    void DrawGameOverTitle()
+    {
+        DrawCenteredText(5, Color::LIGHT_RED,    "  _____                         ____                 ");
+        DrawCenteredText(6, Color::LIGHT_RED,    " / ____|                       / __ \\                ");
+        DrawCenteredText(7, Color::LIGHT_RED,    "| |  __  __ _ _ __ ___   ___  | |  | |_   _____ _ __ ");
+        DrawCenteredText(8, Color::LIGHT_RED,    "| | |_ |/ _` | '_ ` _ \\ / _ \\ | |  | \\ \\ / / _ \\ '__|");
+        DrawCenteredText(9, Color::LIGHT_RED,    "| |__| | (_| | | | | | |  __/ | |__| |\\ V /  __/ |   ");
+        DrawCenteredText(10, Color::LIGHT_RED,   " \\_____|\\__,_|_| |_| |_|\\___|  \\____/  \\_/ \\___|_|   ");
+    }
+
+    GameOverAction ShowGameOverScreen()
+    {
+        system("cls");
+        SetConsoleWindowSize(CONSOLE_WIDTH, CONSOLE_HEIGHT);
+        SetConsoleWindowStyle(true);
+        SetCursorVisible(false);
+        SetConsoleMouseInputDisabled();
+
+        DrawBorder();
+        DrawBox(10, 3, 60, 23, Color::LIGHT_RED);
+        DrawGameOverTitle();
+
+        DrawCenteredText(13, Color::WHITE, "All arrows have been used.");
+        DrawCenteredText(14, Color::LIGHT_GRAY, "You ran out of ammo before clearing every target.");
+
+        DrawText(25, 17, Color::LIGHT_YELLOW, "Reached Stage : ");
+        SetColor(Color::WHITE);
+        cout << currentStage << "/" << MAX_STAGE;
+        SetColor();
+
+        DrawText(25, 18, Color::LIGHT_YELLOW, "Targets Left  : ");
+        SetColor(Color::WHITE);
+        cout << GetRemainingTargetCount() << "/" << targetCount;
+        SetColor();
+
+        DrawText(25, 19, Color::LIGHT_YELLOW, "Ammo Left     : ");
+        SetColor(Color::WHITE);
+        cout << ammo;
+        SetColor();
+
+        DrawCenteredText(22, Color::CYAN, "[ R ] Retry    [ M ] Main Menu    [ ESC ] Exit");
+
+        while (true)
+        {
+            if (_kbhit())
+            {
+                int key = _getch();
+
+                if (key == 'r' || key == 'R')
+                    return GameOverAction::Retry;
+
+                if (key == 'm' || key == 'M')
+                    return GameOverAction::MainMenu;
+
+                if (key == VK_ESCAPE || key == 27)
+                    return GameOverAction::ExitProgram;
+            }
+
+            Sleep(50);
+        }
+    }
+
+    void ResetConsoleForExit()
+    {
+        system("cls");
+        SetCursorVisible(true);
+        GotoXY(0, 0);
+    }
+}
+
+static GameResult RunGame()
 {
     Init();   // 스테이지 1 시작, 스킬 슬롯 초기화 포함
 
@@ -19,7 +179,7 @@ static void RunGame()
         UpdateInput();
 
         if (GetKeyDown(VK_ESCAPE))
-            break;
+            return GameResult::ExitGame;
 
         HandleInput();
         UpdateArrows();
@@ -33,6 +193,10 @@ static void RunGame()
         if (GetKeyDown('3')) TryUseSkill(2);
         if (GetKeyDown('4')) TryUseSkill(3);
 
+        // 화살을 모두 사용했고, 날아가는 화살도 없는데 과녁이 남아 있으면 게임오버
+        if (IsGameOver())
+            return GameResult::GameOver;
+
         // 스테이지 클리어 판정
         if (AllTargetsCleared())
         {
@@ -42,7 +206,7 @@ static void RunGame()
             if (nextStage > MAX_STAGE)
             {
                 ShowGameClear();
-                break;
+                return GameResult::GameClear;
             }
 
             // 카드 3장 중 하나를 골라 스킬 획득
@@ -60,30 +224,51 @@ static void RunGame()
 
 int main()
 {
-    while (true)
+    bool isProgramRunning = true;
+
+    while (isProgramRunning)
     {
         int selectedMenu = ShowMainScreen();
 
         if (selectedMenu == 1)
         {
-            RunGame();
-            break;
-        }
+            while (true)
+            {
+                GameResult result = RunGame();
 
-        if (selectedMenu == 2)
+                if (result == GameResult::GameOver)
+                {
+                    GameOverAction action = ShowGameOverScreen();
+
+                    if (action == GameOverAction::Retry)
+                        continue;
+
+                    if (action == GameOverAction::MainMenu)
+                        break;
+
+                    if (action == GameOverAction::ExitProgram)
+                    {
+                        isProgramRunning = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    isProgramRunning = false;
+                    break;
+                }
+            }
+        }
+        else if (selectedMenu == 2)
         {
             OpenWindowsSoundSettings();
-            continue;
         }
-
-        if (selectedMenu == 3)
-            break;
+        else if (selectedMenu == 3)
+        {
+            isProgramRunning = false;
+        }
     }
 
-    system("cls");
-    SetCursorVisible(true);
-    GotoXY(0, 0);
-    cout << "Game Over!" << endl;
-
+    ResetConsoleForExit();
     return 0;
 }
